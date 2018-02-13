67b32dbbc5b562cc9fc92c331d188461634e00d2
Scott <scotthuston1982@gmail.com>
2018-02-13 14:16:30 -0500

    finished

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/wdi/67/assignments/2055/latest)
+
+1. GET request
+2. 31 ms
+3. 200, this means the page loaded OK.
+4. type: document initiate: other
--- ajax.js
+++ ajax.js
@@ -1,5 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+ var root = document.querySelector('#root');
+ var pong = document.querySelector('#ping-pong');
+ var step3456 = document.querySelector('#step3456');
+ var count = document.querySelector('#count');
+ var step7 = document.querySelector('#step7');
+
+ root.addEventListener('click', function(){
+   $.ajax({
+   url: 'http://first-ajax-api.herokuapp.com/',
+   method: 'GET',
+   dataType: 'text'
+ }).done(function(responseData){
+
+ });
+ });
+ pong.addEventListener('click',function(){
+   $.ajax({
+     url: 'http://first-ajax-api.herokuapp.com/pong',
+     method: 'GET',
+     dataType: 'text'
+   }).done(function(requestData){
+     console.log(requestData)
+     step3456.append(requestData)
+   }).fail(function(){
+     console.log('sorry try again later')
+   }).always(function(){
+     console.log('that was fun')
+   });
+ });
+ count.addEventListener('click', function(){
+   $.ajax({
+     url: 'http://first-ajax-api.herokuapp.com/count',
+     method: 'GET',
+     dataType: 'text'
+   }).done(function(requestData){
+     step7.append(requestData)
+   });
+ });
 
 });
